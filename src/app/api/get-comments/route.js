259593c435fdfb149ba7f17ecf16d4b385ea7d90
zak <zak.goldwasser@gmail.com>
2023-08-25 20:02:59 -0400

    full process .env returned

--- src/app/api/get-comments/route.js
+++ src/app/api/get-comments/route.js
@@ -5,7 +5,7 @@ import { NextResponse } from 'next/server';
 
 export async function POST(request) {
   const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY;
-  return NextResponse.json({ message: YOUTUBE_API_KEY, test_item: 'string' });
+  return NextResponse.json({ message: process.env, test_item: 'string' });
 
   if (!YOUTUBE_API_KEYk) return NextResponse.json({ message: 'No API key' });
   try {
